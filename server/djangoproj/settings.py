298f692846e05355bc9aed71070e648c56c22322
CK <cklau00@gmail.com>
2025-10-22 00:50:29 -0400

    fix lint

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'https://cklau00-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
+ALLOWED_HOSTS = ['localhost', 
+                 'https://*.cognitiveclass.ai']
 CSRF_TRUSTED_ORIGINS = ['https://*.cognitiveclass.ai']
 
 REST_FRAMEWORK = {
@@ -62,9 +63,9 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR,'frontend/static'),
-            os.path.join(BASE_DIR,'frontend/build'),
-            os.path.join(BASE_DIR,'frontend/build/static'),
+            os.path.join(BASE_DIR, 'frontend/static'),
+            os.path.join(BASE_DIR, 'frontend/build'),
+            os.path.join(BASE_DIR, 'frontend/build/static'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -94,7 +95,7 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.' \
+        'django.contrib.auth.password_validation.'
         'UserAttributeSimilarityValidator',
     },
     {
