a227b56a5418c3249922bb82b7ce2dff9bfc54d5
CK <cklau00@gmail.com>
2025-10-22 00:51:32 -0400

    fix lint

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 
+ALLOWED_HOSTS = ['localhost',
                  'https://*.cognitiveclass.ai']
 CSRF_TRUSTED_ORIGINS = ['https://*.cognitiveclass.ai']
 
