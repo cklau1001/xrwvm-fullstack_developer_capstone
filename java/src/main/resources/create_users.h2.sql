5e482a37022d7134a2620e94d10b443bdf9122ba
cklau00 <cklau00@gmail.com>
2025-11-13 11:57:48 +0800

    Update application.yml

--- java/src/main/resources/create_users.h2.sql
+++ java/src/main/resources/create_users.h2.sql
@@ -1,4 +1,4 @@
 merge into APP_USER (USERNAME, PASSWORD, FIRSTNAME, LASTNAME, EMAIL, CREATED_DATE, CREATED_BY)
-values('admin', 'abc12345', 'adminF', 'adminL', 'admin@abc.com', now(), 'SYSTEM');
+values('admin', <need-update>, 'adminF', 'adminL', 'admin@abc.com', now(), 'SYSTEM');
 merge into APP_ROLE(USERNAME, ROLE, CREATED_DATE, CREATED_BY) values ('admin', 'ADMIN', now(), 'SYSTEM');
 
